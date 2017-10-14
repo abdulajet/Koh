857d3e669eee4876ba63e91d0e6b86cd472ddd51
abdulajet <abdulrapidz@live.com>
2017-10-14 16:50:02 +0200

    get tweet and reply

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 require('dotenv').config()
-var Twitter = require('twitter');
-var Express = require('express');
+const Twitter = require('twitter');
+const Express = require('express');
 
-var twitterClient = new Twitter({
+const twitterUsername = "@abdulajet"
+
+const twitterClient = new Twitter({
   consumer_key: process.env.C_KEY,
   consumer_secret: process.env.C_SECRET,
   access_token_key: process.env.A_KEY,
   access_token_secret: process.env.A_SECRET
 });
 
-twitterClient.get('statuses/mentions_timeline', function(error, tweets, response) {
-  if(error) throw error;
-  console.log(tweets);  // The favorites.
-  //console.log(response);  // Raw response object.
+// You can also get the stream in a callback if you prefer.
+const stream = twitterClient.stream('statuses/filter', {track: twitterUsername});
+
+stream.on('data', function(event) {
+  var handle = event.user.screen_name;
+  var tweetId = event.id_str;
+  var img = event.entities.media[0].media_url_https;
+  stealFace(handle, tweetId, img);
 });
+
+stream.on('error', function(error) {
+  console.log(error);
+});
+
+function stealFace(handle, tweetId, img) {
+  //img as a url pls
+  reply(handle, tweetId, img)
+}
+
+function reply(handle, tweetId, img) {
+  twitterClient.post('statuses/update', {status: `@${handle} This is an automated test ${img}`, in_reply_to_status_id: tweetId}, function(error, tweet, response) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log(tweet);
+    }
+  });
+}
