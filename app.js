59ce6d3c9e311b8ed4ee5526a7ebc1aaf3318276
Anosh Malik <me@ano.sh>
2017-10-14 23:28:05 +0200

    do work
    Signed-off-by: Anosh Malik <me@ano.sh>

--- app.js
+++ app.js
@@ -1,39 +1,117 @@
-require('dotenv').config()
+require('dotenv').config();
 const twitter = require('twitter');
-
+const fs = require('fs');
+const request = require('request');
+const gm = require('gm');
 const Twitter = new twitter({
   consumer_key: process.env.C_KEY,
   consumer_secret: process.env.C_SECRET,
   access_token_key: process.env.A_KEY,
   access_token_secret: process.env.A_SECRET
 });
-
 const track = process.env.TWITTER_HANDLE;
+const AZURE_KEY = process.env.AZURE_KEY;
+const FACE_BASE_URI = process.env.FACE_BASE_URI;
 
 // You can also get the stream in a callback if you prefer.
-const stream = twitterClient.stream('statuses/filter', { track });
+const stream = Twitter.stream('statuses/filter', { track });
 
 stream.on('data', event => {
   const handle = event.user.screen_name;
   const tweetId = event.id_str;
-  const img = event.entities.media[0].media_url_https;
-  return stealFace(handle, tweetId, img);
+  const imgUrl = event.entities.media[0].media_url_https;
+  return stealFace(handle, tweetId, imgUrl);
 });
 
 stream.on('error', error => console.log(error));
 
-const stealFace = (handle, tweetId, img) => {
-  //img as a url pls
-  return reply(handle, tweetId, img);
+const stealFace = (handle, tweetId, url) => {
+  request({
+    method: 'POST',
+    uri: FACE_BASE_URI,
+    qs: {
+      returnFaceId: true,
+      returnFaceLandmarks: false
+    },
+    json: true,
+    headers: {
+      'Ocp-Apim-Subscription-Key': AZURE_KEY
+    },
+    body: {
+      url
+    }
+  }, (err, resp, body) => {
+    if (!err) {
+      let faceRectangle = body[0].faceRectangle;
+      request({
+        method: 'GET',
+        uri: url,
+        encoding: null
+      }, (err, resp, body) => {
+        if (!err) {
+          console.log('1');
+          gm(body)
+          .crop(faceRectangle.width, faceRectangle.height, faceRectangle.left, faceRectangle.top)
+          .write('./1.jpeg', (err) => {
+            if(!err) {
+              console.log('2');
+              request({
+                method: 'POST',
+                uri: FACE_BASE_URI,
+                qs: {
+                  returnFaceId: true,
+                  returnFaceLandmarks: false
+                },
+                json: true,
+                headers: {
+                  'Ocp-Apim-Subscription-Key': AZURE_KEY
+                },
+                body: {
+                  url: 'https://c1.staticflickr.com/4/3798/13869386723_a8ec119485_b.jpg'
+                }
+              }, (err, resp, body) => {
+                if (!err) {
+                  console.log('3');
+                  faceRectangle = body[0].faceRectangle;
+                  console.log(JSON.stringify(faceRectangle));
+                  request({
+                    method: 'GET',
+                    uri: 'https://c1.staticflickr.com/4/3798/13869386723_a8ec119485_b.jpg',
+                    encoding: null
+                  }, (err, resp, body) => {
+                    if (!err) {
+                      console.log('4');
+                      gm(body)
+                      .draw(`image Over ${faceRectangle.left},${faceRectangle.top} ${faceRectangle.width},${faceRectangle.height} "./1.jpeg"`)
+                      .write('./2.jpeg', (err) => {
+                        if (!err) {
+                          console.log('5');
+                          return reply(handle, tweetId, buffer);
+                        }
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
 }
 
-const reply = (handle, in_reply_to_status_id, img) => {
-  const status = `@${handle} This is an automated test ${img}`;
-  twitterClient.post('statuses/update', { status, in_reply_to_status_id }, (error, tweet, response) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(tweet);
+const reply = (handle, in_reply_to_status_id, media) => {
+  const status = `@${handle} Here is your image!`;
+  // Make post request on media endpoint. Pass file data as media parameter
+  Twitter.post('media/upload', { media }, function(error, img, response) {
+    if (!error) {
+      const media_ids = img.media_id_string;
+      Twitter.post('statuses/update', { status, in_reply_to_status_id, media_ids }, function(error, tweet, response) {
+        if (!error) {
+          console.log(tweet);
+        }
+      });
     }
   });
 }
