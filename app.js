969c5596a2d09897f023067f7182d5549df79076
Anosh Malik <me@ano.sh>
2017-10-14 18:41:26 +0200

    Clean up
    Signed-off-by: Anosh Malik <me@ano.sh>

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 require('dotenv').config()
-const Twitter = require('twitter');
-const Express = require('express');
+const twitter = require('twitter');
 
-const twitterUsername = "@abdulajet"
-
-const twitterClient = new Twitter({
+const Twitter = new twitter({
   consumer_key: process.env.C_KEY,
   consumer_secret: process.env.C_SECRET,
   access_token_key: process.env.A_KEY,
   access_token_secret: process.env.A_SECRET
 });
 
+const track = process.env.TWITTER_HANDLE;
+
 // You can also get the stream in a callback if you prefer.
-const stream = twitterClient.stream('statuses/filter', {track: twitterUsername});
+const stream = twitterClient.stream('statuses/filter', { track });
 
-stream.on('data', function(event) {
-  var handle = event.user.screen_name;
-  var tweetId = event.id_str;
-  var img = event.entities.media[0].media_url_https;
-  stealFace(handle, tweetId, img);
+stream.on('data', event => {
+  const handle = event.user.screen_name;
+  const tweetId = event.id_str;
+  const img = event.entities.media[0].media_url_https;
+  return stealFace(handle, tweetId, img);
 });
 
-stream.on('error', function(error) {
-  console.log(error);
-});
+stream.on('error', error => console.log(error));
 
-function stealFace(handle, tweetId, img) {
+const stealFace = (handle, tweetId, img) => {
   //img as a url pls
-  reply(handle, tweetId, img)
+  return reply(handle, tweetId, img);
 }
 
-function reply(handle, tweetId, img) {
-  twitterClient.post('statuses/update', {status: `@${handle} This is an automated test ${img}`, in_reply_to_status_id: tweetId}, function(error, tweet, response) {
+const reply = (handle, in_reply_to_status_id, img) => {
+  const status = `@${handle} This is an automated test ${img}`;
+  twitterClient.post('statuses/update', { status, in_reply_to_status_id }, (error, tweet, response) => {
     if (error) {
-      console.log(error)
+      console.log(error);
     } else {
       console.log(tweet);
     }
